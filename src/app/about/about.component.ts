49673f2f7534a52adb815c13cee18fa3a22700d0
@bibs <bibdollard@hotmail.com>
2018-03-25 16:55:14 +0000

    first commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+  goals: any;
+  constructor(private router: Router, private route: ActivatedRoute, private _data: DataService) {
+   this.route.params.subscribe(res => console.log(res.id));
+  }
+
+  ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
+  }
+  sendMeHome() {
+    this.router.navigate(['']);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Component } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { DataService } from './data.service';
+// import { AppRoutingModule} from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+// import { Route } from '@angular/compiler/src/core';
 
 
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about/:id', component: AboutComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes, {enableTracing: true})
   ],
-  providers: [],
+  exports: [ RouterModule ],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
